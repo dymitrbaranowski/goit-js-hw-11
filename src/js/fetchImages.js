39e94a46d8b9020837044fff8fdfcdcea7a5bc95
dymitrbaranowski <dmitribaranowski51@gmail.com>
2023-07-24 21:32:30 +0300

    fox2

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,22 +1,12 @@
 import axios from 'axios';
+export { fetchImages };
 
-axios.defaults.headers.common['key'] = '38440528-27ad43a15fe64cab61d6047d1';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '38440528-27ad43a15fe64cab61d6047d1';
 
-export const fetchImages = async (inputValue, pageNr) => {
-  return axios.get(
-    `/q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}/`
-  )
-    .then(async response => {
-      if (response.status !== 200) {
-        if (response.status !== 200) {
-          return [];
-        }
-        throw new Error(response.status);
-      }
-      return await response.data;
-    })
-    .catch(error => {
-      console.error(error);
-    });
-};
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response;
+}
